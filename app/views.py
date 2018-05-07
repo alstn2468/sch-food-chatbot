a6c99bbb4620f1d9f0b232abcb061e98aa887031
alstn2468 <alstn2468_@naver.com>
2018-05-08 08:46:22 +0900

    2018-05-08

--- app/views.py
+++ app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def keyboard(request) :
+	return JsonResponse (
+		{
+		'type' : 'buttons',
+		'buttons' : ['button1', 'button2', 'button3']
+		}
+	)
--- my_chatbot/urls.py
+++ my_chatbot/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'admin/', admin.site.urls),
+	url(r'^keyboard', views.keyboard)
 ]
