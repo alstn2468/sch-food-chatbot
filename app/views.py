6299bf08e187a729259574bb47d20a01863d2c1c
alstn2468 <alstn2468_@naver.com>
2018-05-11 23:14:17 +0900

    2018-05-11

--- app/views.py
+++ app/views.py
@@ -1,27 +1,67 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
 
 def keyboard(request) :
+
 	return JsonResponse (
 		{
 		'type' : 'buttons',
-		'buttons' : ['button1', 'button2', 'button3']
+		'buttons' : ['학색', '내일의 학식', '시간별 학식']
 		}
 	)
 
+@csrf_exempt
 def message(request) :
-	return JsonResponse (
-		{
-			'message' :
+
+	json_str = (request.body).decode('utf-8')
+	received_json = json.loads(json_str)
+	content_name = received_json['content']
+
+	if content_name == '학식' :
+		return JsonResponse (
 			{
-				'text' : 'Input the sentence that you want.'
-			},
-			'keyboard' :
-			{
-				'type' : 'button',
-				'buttons' : ['button1', 'button2', 'button3', 'button4', 'button5']
+				'message' :
+				{
+					'text' : '선택한 버튼 : ' + content_name
+				},
+				'keyboard' :
+				{
+					'type' : 'button',
+					'buttons' : ['학식', '내일의 학식', '시간별 학식']
+				}
 			}
-		}
-	)
+		)
+
+	elif content_name == '내일의 학식' :
+		return JsonResponse (
+			{
+				'message' :
+				{
+					'text' : '선택한 버튼 : ' + content_name
+				},
+				'keyboard' :
+				{
+					'type' : 'button',
+					'buttons' : ['학식', '내일의 학식', '시간별 학식']
+				}
+			}
+		)
+
+	elif content_name == '시간별 학식' :
+		return JsonResponse (
+			{
+				'message' :
+				{
+					'text' : '선택한 버튼 : ' + content_name
+				},
+				'keyboard' :
+				{
+					'type' : 'button',
+					'buttons' : ['학식', '내일의 학식', '시간별 학식']
+				}
+			}
+		)
